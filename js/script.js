1de3dc45f5b5ecc9f19f0ec4289beefc07cf0db8
Elvis <elvis.melkic@gmail.com>
2019-02-16 20:17:17 +0100

    fixed responsiveness and mobile navigation

--- js/script.js
+++ js/script.js
@@ -1,21 +1,41 @@
-$(function() {
-    $(".nav__sub-list").hover(function () {
-        $(".nav__sub-list")
-            .prev(".nav__link")
-            .toggleClass("nav__link--active");
-    }, function () {
-        $(".nav__sub-list")
-            .prev(".nav__link")
-            .toggleClass("nav__link--active");
+// JQUERY CODE
+// $(function() {
+//     $(".nav__sub-list").hover(function () {
+//         $(".nav__sub-list")
+//             .prev(".nav__link")
+//             .toggleClass("nav__link--active");
+//     }, function () {
+//         $(".nav__sub-list")
+//             .prev(".nav__link")
+//             .toggleClass("nav__link--active");
+//     });
+
+//     $("#mobile-services").click(function () {
+//         $(this)
+//             .children(".mobile-nav__link")
+//             .children()
+//             .toggleClass("mobile-nav__open-caret");
+//         $(this)
+//             .children(".mobile-nav__sub-list")
+//             .toggleClass("mobile-nav__sub-list--visible");
+//     });
+// });
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    var subList = document.querySelector(".nav__sub-list");
+
+    subList.addEventListener("mouseover", function(event) {
+        subList.previousElementSibling.classList.toggle("nav__link--active");
     });
 
-    $("#mobile-services").click(function () {
-        $(this)
-            .children(".mobile-nav__link")
-            .children()
-            .toggleClass("mobile-nav__open-caret");
-        $(this)
-            .children(".mobile-nav__sub-list")
-            .toggleClass("mobile-nav__sub-list--visible");
+    subList.addEventListener("mouseout", function(event) {
+        subList.previousElementSibling.classList.toggle("nav__link--active");
+    });
+
+    var mobileNavServices = document.getElementById("mobile-services");
+
+    mobileNavServices.addEventListener("click", function(event) {
+        mobileNavServices.children[0].children[0].classList.toggle("mobile-nav__open-caret");
+        mobileNavServices.children[1].classList.toggle("mobile-nav__sub-list--visible");
     });
 });
